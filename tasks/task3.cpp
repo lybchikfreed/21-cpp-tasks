fadf16fee12b79274514c99d52e9fb117a393211
freed <Lybchikf777@gmail.com>
2021-01-02 23:01:58 +0200

    added prints

--- tasks/task3.cpp
+++ tasks/task3.cpp
@@ -4,15 +4,19 @@ const float g = 9.8;
 float Speed0(float s, float t) {
 	return (s - g * t * t / 2.0) / t;
 }
+
 float fallHeight(float U0, float t) {
 	return (U0 * t + g * t * t / 2.0);
-
 }
+
 int task3() {
 	float k,s,p;
+	printf_s("input k: ");
 	std::cin >> k;
+	printf_s("input s: ");
 	std::cin >> s;
+	printf_s("input p: ");
 	std::cin >> p;
-	std::cout << fallHeight(Speed0(s, k * 60), p)<<std::endl;
+	printf_s("object will travel %.2f m in %.2f s \n", fallHeight(Speed0(s, k * 60), p),p);
 	return 1;
 }
