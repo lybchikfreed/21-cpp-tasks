fe4084a1ed9bfd5e2d98c7381abbf10771f2f908
freed <Lybchikf777@gmail.com>
2021-01-02 17:04:59 +0200

    1st task

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 
 
 int (*tasksarr[])() = { task1, task2 };
+
 int main() {
 
-	int a;
+	unsigned int a;
 	std::cout << "Enter task number: " << std::endl;
 	std::cin >> a;
+	if (a > 2||a==0) {
+		std::cout << "Task does not exist" << std::endl;
+		return 0;
+	}
 	(*tasksarr[a-1])();
 
 	system("pause");
--- tasks/task1.cpp
+++ tasks/task1.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
-
+#include <cmath>
+char* intToB(int i) {
+	char *bin = new char[30];
+	itoa(abs(i), bin, 2);
+	return bin;
+}
 int task1() {
-	std::cout << 1 << std::endl;
+	int k,a,b;
+	char *notarr[]= { "not ","" };
+	bool inrng;
+	printf_s("input k: ");
+	std::cin >> k;
+	printf_s("input a: ");
+	std::cin >> a;
+	printf_s("input b: ");
+	std::cin >> b;
+	if (k > a && k < b)
+		inrng = 1;
+	else
+		inrng= 0;
+	char* kbin = intToB(k),*abin = intToB(a), * bbin = intToB(b);
+	
+	printf_s("%s is %sin range(%s..%s)\n", kbin, notarr[inrng], abin, bbin);
+	printf_s("%o is %sin range(%o..%o)\n",k,notarr[inrng],a,b); 
+	printf_s("%X is %sin range(%X..%X)\n", k, notarr[inrng], a, b);
+	// doesn't work with negative numbers
 	return 1;
 }
